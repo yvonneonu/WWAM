26b4d944102a47ca995e037b60d41d537d9be9a2
Bamidele Emmanuel <bamideleomonayin@gmail.com>
2021-03-07 13:43:25 +0100

    showing the lines for the features and making it dissappaer

--- app/src/main/java/com/example/waam/BecomeAMemberFragment.java
+++ app/src/main/java/com/example/waam/BecomeAMemberFragment.java
@@ -10,6 +10,7 @@ import androidx.fragment.app.Fragment;
 import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.style.BulletSpan;
+import android.text.style.ForegroundColorSpan;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -20,10 +21,8 @@ import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.LinearLayout;
-import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.Toolbar;
 
 import com.google.android.material.card.MaterialCardView;
 
@@ -41,10 +40,16 @@ public class BecomeAMemberFragment extends Fragment implements View.OnClickListe
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
     private MaterialCardView linearLayoutOne, linearLayoutTwo, linearLayoutThree, linearLayoutFour;
-    private LinearLayout linearLayoutFive, layoutPlan;
+    private LinearLayout layoutPlan;
+    private LinearLayout layoutPlantwo;
+    private LinearLayout layoutPlanthree;
+    private LinearLayout layoutPlanfour;
+    private View firstView, secondView, thirdView, fourthView;
     private CheckBox radioButtonone;
     private CheckBox radioButtontwo;
     private CheckBox radioButtonthree;
+    TextView[] firstFeature, secondFeature, thirdFeature, fourFeature;
+    private View[] arrayView;
     //private MenuItem menuItem;
 
     // TODO: Rename and change types of parameters
@@ -85,22 +90,54 @@ public class BecomeAMemberFragment extends Fragment implements View.OnClickListe
         setHasOptionsMenu(true);
 
         SpannableString string = new SpannableString("Bullet point");
-        string.setSpan(new BulletSpan(), 0, 12, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        string.setSpan(new BulletSpan(),0, 12, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
 
 
         linearLayoutOne = view.findViewById(R.id.layone);
         linearLayoutTwo = view.findViewById(R.id.laytwo);
         linearLayoutThree = view.findViewById(R.id.laythree);
         linearLayoutFour = view.findViewById(R.id.layfour);
-        linearLayoutFive = view.findViewById(R.id.purchase);
+        LinearLayout linearLayoutFive = view.findViewById(R.id.purchase);
 
         layoutPlan = view.findViewById(R.id.layoutplan);
-
+        layoutPlantwo = view.findViewById(R.id.layoutplantwo);
+        layoutPlanthree = view.findViewById(R.id.layoutplanthree);
+        layoutPlanfour = view.findViewById(R.id.layoutplanfour);
 
         TextView firstTxt = view.findViewById(R.id.textView28);
         TextView secondTxt = view.findViewById(R.id.textView29);
         TextView thirdTxt = view.findViewById(R.id.textView32);
         TextView fourthTxt = view.findViewById(R.id.textView33);
+        TextView firsttin = view.findViewById(R.id.textView34fourtin);
+        TextView secondtin = view.findViewById(R.id.textView35fourtin);
+        TextView thirdtin = view.findViewById(R.id.textView36fourtin);
+        TextView fourtin = view.findViewById(R.id.textView37fourtin);
+
+        TextView firsttwe = view.findViewById(R.id.textView38twe);
+        TextView secondtwe = view.findViewById(R.id.textView39twe);
+        TextView thirdtwe = view.findViewById(R.id.textView40twe);
+        TextView fourttwe = view.findViewById(R.id.textView41twe);
+
+        TextView firsthun = view.findViewById(R.id.textView42hun);
+        TextView secondhun = view.findViewById(R.id.textView43hun);
+        TextView thirdhun = view.findViewById(R.id.textView44hun);
+        TextView fourthun = view.findViewById(R.id.textView45hun);
+
+
+        firstView = view.findViewById(R.id.view7);
+        secondView = view.findViewById(R.id.view8);
+        thirdView = view.findViewById(R.id.view9);
+        fourthView = view.findViewById(R.id.view11);
+        firstFeature = new TextView[]{view.findViewById(R.id.textViewone),view.findViewById(R.id.textViewtwo),view.findViewById(R.id.textViewthree)};
+        secondFeature = new TextView[]{view.findViewById(R.id.textViewfour),view.findViewById(R.id.textViewfive),view.findViewById(R.id.textViewsix)};
+        thirdFeature = new TextView[]{view.findViewById(R.id.textViewseven),view.findViewById(R.id.textVieweight),view.findViewById(R.id.textViewnine)};
+        fourFeature = new TextView[]{view.findViewById(R.id.textView19),view.findViewById(R.id.textView21),view.findViewById(R.id.textView20)};
+
+        TextView[] arrayTin = new TextView[]{firsttin,secondtin,thirdtin,fourtin};
+        TextView[] arrayTwe = new TextView[]{firsttwe,secondtwe,thirdtwe,fourttwe};
+        TextView[] arrayHun = new TextView[]{firsthun,secondhun,thirdhun,fourthun};
+
+        arrayView = new View[]{firstView,secondView, thirdView,fourthView};
 
         radioButtonone = view.findViewById(R.id.radioButtonone);
         radioButtontwo = view.findViewById(R.id.radioButtontwo);
@@ -117,6 +154,18 @@ public class BecomeAMemberFragment extends Fragment implements View.OnClickListe
         secondTxt.setText(string);
         thirdTxt.setText(string);
         fourthTxt.setText(string);
+
+       for(int i= 0 ; i < arrayTin.length ; i++){
+           arrayTin[i].setText(string);
+       }
+
+        for(int i= 0 ; i < arrayTin.length ; i++){
+            arrayTwe[i].setText(string);
+        }
+
+        for(int i= 0 ; i < arrayTin.length ; i++){
+            arrayHun[i].setText(string);
+        }
 
 
         radioButtonone.setOnCheckedChangeListener(this);
@@ -152,18 +201,28 @@ public class BecomeAMemberFragment extends Fragment implements View.OnClickListe
 
         switch (v.getId()){
             case layoutOne:
-                setClicked(linearLayoutTwo,linearLayoutThree,linearLayoutFour,linearLayoutOne);
+                setClicked(linearLayoutTwo,linearLayoutThree,linearLayoutFour
+                        ,linearLayoutOne,layoutPlantwo,layoutPlanthree,
+                        layoutPlanfour,layoutPlan,secondFeature,thirdFeature,fourFeature,firstFeature,firstView);
                 break;
             case layoutTwo:
-                setClicked(linearLayoutOne,linearLayoutThree,linearLayoutFour,linearLayoutTwo);
+                setClicked(linearLayoutOne,linearLayoutThree,linearLayoutFour,
+                        linearLayoutTwo,layoutPlan,layoutPlanthree,layoutPlanfour,
+                        layoutPlantwo,firstFeature,thirdFeature,fourFeature,secondFeature,secondView);
                 break;
 
             case layoutThree:
-                setClicked(linearLayoutOne,linearLayoutTwo,linearLayoutFour,linearLayoutThree);
+                setClicked(linearLayoutOne,linearLayoutTwo,linearLayoutFour,
+                        linearLayoutThree,layoutPlan,layoutPlantwo,
+                        layoutPlanfour,layoutPlanthree,firstFeature,secondFeature,
+                        fourFeature,thirdFeature,thirdView);
                 break;
 
             case layoutFour:
-                setClicked(linearLayoutOne,linearLayoutTwo,linearLayoutThree,linearLayoutFour);
+                setClicked(linearLayoutOne,linearLayoutTwo,linearLayoutThree,
+                        linearLayoutFour,layoutPlan,layoutPlantwo,
+                        layoutPlanthree,layoutPlanfour,firstFeature,
+                        secondFeature,thirdFeature,fourFeature,fourthView);
                 break;
 
             case purchase:
@@ -173,11 +232,44 @@ public class BecomeAMemberFragment extends Fragment implements View.OnClickListe
     }
 
 
-    private void setClicked(MaterialCardView first, MaterialCardView second, MaterialCardView third,MaterialCardView original){
-        first.setBackgroundResource(R.drawable.price_box_bg);
-        second.setBackgroundResource(R.drawable.price_box_bg);
-        third.setBackgroundResource(R.drawable.price_box_bg);
-        original.setBackgroundResource(R.drawable.price_box_bg_full);
+    private void setClicked(MaterialCardView first, MaterialCardView second,
+                            MaterialCardView third,MaterialCardView original,
+                            LinearLayout linearLayoutone,LinearLayout linearLayouttwo,
+                            LinearLayout linearLayoutthree,LinearLayout linearLayoutOriginal,
+                            TextView[] arrayOne, TextView[] arrayTwo, TextView[] arrayThree, TextView[] arrayReal,View view){
+        first.setBackgroundColor(Objects.requireNonNull(getActivity()).getResources().getColor(R.color.white));
+        second.setBackgroundColor(getActivity().getResources().getColor(R.color.white));
+        third.setBackgroundColor(getActivity().getResources().getColor(R.color.white));
+        original.setBackgroundColor(getActivity().getResources().getColor(R.color.priceBackgrouond));
+        linearLayoutone.setVisibility(View.GONE);
+        linearLayouttwo.setVisibility(View.GONE);
+        linearLayoutthree.setVisibility(View.GONE);
+        linearLayoutOriginal.setVisibility(View.VISIBLE);
+
+        for (View value : arrayView) {
+            if (value == view) {
+                value.setVisibility(View.VISIBLE);
+            } else {
+                value.setVisibility(View.GONE);
+            }
+        }
+
+        for (TextView textView : arrayOne) {
+            textView.setTextColor(getActivity().getResources().getColor(R.color.black));
+        }
+
+        for(int i = 0 ; i < arrayOne.length ; i++){
+            arrayTwo[i].setTextColor(getActivity().getResources().getColor(R.color.black));
+        }
+
+        for(int i = 0 ; i < arrayOne.length ; i++){
+            arrayThree[i].setTextColor(getActivity().getResources().getColor(R.color.black));
+        }
+
+        for(int i = 0 ; i < arrayOne.length ; i++){
+            arrayReal[i].setTextColor(getActivity().getResources().getColor(R.color.white));
+        }
+
     }
 
     @Override
