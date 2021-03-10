bdb9b931efa70fa058e6aa08e420b197c958729d
Bamidele Emmanuel <bamideleomonayin@gmail.com>
2021-03-10 20:05:41 +0100

    adding a swipe nxt

--- app/src/main/java/com/example/waam/finalProfile.java
+++ app/src/main/java/com/example/waam/finalProfile.java
@@ -2,13 +2,26 @@ package com.example.waam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class finalProfile extends AppCompatActivity {
 
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_final_profile);
+
+        textView = findViewById(R.id.textView34);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(finalProfile.this,DrawelayoutActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
