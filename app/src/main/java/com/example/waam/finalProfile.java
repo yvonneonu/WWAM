28d514bf8586bc41e745f2c91e60c4147c9c9273
Bamidele Emmanuel <bamideleomonayin@gmail.com>
2021-03-15 00:21:42 +0100

    Go and sleep yvonne(tree)

--- app/src/main/java/com/example/waam/finalProfile.java
+++ app/src/main/java/com/example/waam/finalProfile.java
@@ -22,10 +22,10 @@ public class finalProfile extends AppCompatActivity {
         String token = getIntent().getStringExtra("token");
         FetchSpinnerValues.getSpinnerValues().fetchEducation(new FetchSpinnerValues.EducationListener() {
             @Override
-            public void onEducationListener(List<String> userSchool) {
-                ArrayAdapter<String> areasAdapter = new ArrayAdapter<String>(finalProfile.this, android.R.layout.simple_spinner_item, userSchool);
-                areasAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                spinner.setAdapter(areasAdapter);
+            public void onEducationListener(List<String> qualification) {
+                ArrayAdapter<String> qualificationAdapter = new ArrayAdapter<String>(finalProfile.this, android.R.layout.simple_spinner_item, qualification);
+                qualificationAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinner.setAdapter(qualificationAdapter);
             }
         },token);
     }
