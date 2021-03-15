ffe787a4b9a89569f45755171299cf3fd6135cf8
yvonneonu <yvonneonuorah@gmail.com>
2021-03-15 23:05:00 +0100

    done done

--- app/src/main/java/com/example/waam/LookingFor.java
+++ app/src/main/java/com/example/waam/LookingFor.java
@@ -3,18 +3,35 @@ package com.example.waam;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 public class LookingFor extends AppCompatActivity {
 
     private TextView textView;
+    private ImageView image;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_looking_for);
         textView = findViewById(R.id.textView34);
+        image = findViewById(R.id.imageView12);
+
+        String imageUri = getIntent().getStringExtra("image");
+
+
+        Glide.with(this)
+                .asBitmap()
+                .circleCrop()
+                .load(Uri.parse(imageUri))
+                .into(image);
+
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
