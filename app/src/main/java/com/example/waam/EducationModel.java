f4006efeb9a8fd9fc6b8690525a16a3b4118ff46
yvonneonu <yvonneonuorah@gmail.com>
2021-03-18 00:12:57 +0100

    dont

--- app/src/main/java/com/example/waam/EducationModel.java
+++ app/src/main/java/com/example/waam/EducationModel.java
@@ -4,10 +4,6 @@ public class EducationModel {
     private String id;
     private String name;
 
-    public EducationModel(String id, String name) {
-        this.id = id;
-        this.name = name;
-    }
 
     public String getId() {
         return id;
