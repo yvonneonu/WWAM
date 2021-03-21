54947c82601b9fc0798fc917201d7991360acf78
yvonneonu <yvonneonuorah@gmail.com>
2021-03-21 03:19:17 +0100

    thank you very much,

--- app/src/main/java/com/example/waam/MainActivity.java
+++ app/src/main/java/com/example/waam/MainActivity.java
@@ -1,15 +1,19 @@
 package com.example.waam;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
 
 }
