9849265a67b5930fec9587ae43fc41826bc5b9f1
yvonneonu <yvonneonuorah@gmail.com>
2021-03-29 20:01:24 +0100

    design done

--- app/src/main/java/com/example/waam/DiscoverEvent.java
+++ app/src/main/java/com/example/waam/DiscoverEvent.java
@@ -3,12 +3,29 @@ package com.example.waam;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DiscoverEvent extends AppCompatActivity {
+    private FragmentManager mFragmentManager;
+    private List<Fragment> mFragments = new ArrayList<>();
+    private List<String> mManagerNames = new ArrayList<>();
+
+    private Fragment mCurrentFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_discover_event);
+        mFragmentManager = getSupportFragmentManager();
+        intFragments();
+    }
+
+    private void intFragments() {
+
     }
 }
