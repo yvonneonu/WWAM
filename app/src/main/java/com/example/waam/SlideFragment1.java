d495ae0e009d579f306efa1d6a4913b0cf0b31e3
yvonneonu <yvonneonuorah@gmail.com>
2021-04-06 23:42:40 +0100

    Merge branch 'testpayment' into AccountProfile
    # Conflicts:
    #	app/src/main/java/com/example/waam/finalProfile.java

--- app/src/main/java/com/example/waam/SlideFragment1.java
+++ app/src/main/java/com/example/waam/SlideFragment1.java
@@ -1,23 +1,18 @@
 package com.example.waam;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link DiscoverFragment#newInstance} factory method to
+ * Use the {@link SlideFragment1#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class DiscoverFragment extends Fragment {
-
+public class SlideFragment1 extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -28,7 +23,7 @@ public class DiscoverFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public DiscoverFragment() {
+    public SlideFragment1() {
         // Required empty public constructor
     }
 
@@ -38,11 +33,11 @@ public class DiscoverFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment DiscoverFragment.
+     * @return A new instance of fragment SlideFragment1.
      */
     // TODO: Rename and change types and number of parameters
-    public static DiscoverFragment newInstance(String param1, String param2) {
-        DiscoverFragment fragment = new DiscoverFragment();
+    public static SlideFragment1 newInstance(String param1, String param2) {
+        SlideFragment1 fragment = new SlideFragment1();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -63,29 +58,6 @@ public class DiscoverFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_discover, container, false);
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        TextView textView = view.findViewById(R.id.events);
-        TextView textView1 = view.findViewById(R.id.matches);
-
-        textView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), DiscoverEvent.class);
-                startActivity(intent);
-            }
-        });
-
-        textView1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), MatchFragment.class);
-                startActivity(intent);
-            }
-        });
+        return inflater.inflate(R.layout.fragment_slide1, container, false);
     }
 }
