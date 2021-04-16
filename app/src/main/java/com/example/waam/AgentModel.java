b328c7647b12aec7a84ecf3c1651dfee063045e7
yvonneonu <yvonneonuorah@gmail.com>
2021-04-16 23:53:01 +0100

    Merge branch 'testpayment' into AccountProfile
    # Conflicts:
    #	app/src/main/java/com/example/waam/finalProfile.java

---
+++ app/src/main/java/com/example/waam/AgentModel.java
@@ -0,0 +1,49 @@
+package com.example.waam;
+
+import java.io.Serializable;
+
+public class AgentModel implements Serializable {
+    private int image;
+    private String name;
+    private String rating;
+    private String rating1;
+
+    public AgentModel(int image, String name, String rating, String rating1) {
+        this.image = image;
+        this.name = name;
+        this.rating = rating;
+        this.rating1 = rating1;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getRating1() {
+        return rating1;
+    }
+
+    public void setRating1(String rating1) {
+        this.rating1 = rating1;
+    }
+}
