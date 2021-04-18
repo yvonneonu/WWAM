a629e1ad57a141a74242abd1daba17500c97e875
Bamidele Emmanuel <bamideleomonayin@gmail.com>
2021-04-18 13:20:00 +0100

    Setting up the view in profile

--- app/src/main/java/com/example/waam/ConnectedFriendsFragment.java
+++ app/src/main/java/com/example/waam/ConnectedFriendsFragment.java
@@ -3,17 +3,19 @@ package com.example.waam;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link ConnectedFriendsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ConnectedFriendsFragment extends Fragment {
+public class ConnectedFriendsFragment extends Fragment implements View.OnClickListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -53,12 +55,60 @@ public class ConnectedFriendsFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        Fragment fr =new VideoPicFragment();
+        FragmentTransaction ft = getChildFragmentManager().beginTransaction();
+        ft.add(R.id.profileframe,fr)
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                .commit();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_connected_friends, container, false);
+        View view = inflater.inflate(R.layout.fragment_connected_friends, container, false);
+        ImageView videopic = view.findViewById(R.id.videopic);
+        ImageView aboutsef = view.findViewById(R.id.aboutsef);
+        ImageView interest = view.findViewById(R.id.interest);
+        ImageView friends = view.findViewById(R.id.friends);
+
+        videopic.setOnClickListener(this);
+        aboutsef.setOnClickListener(this);
+        interest.setOnClickListener(this);
+        friends.setOnClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        final int vid = R.id.videopic;
+        final  int aboutsef = R.id.aboutsef;
+        final int interest = R.id.interest;
+        final int friends = R.id.friends;
+        final int profileFrame = R.id.profileframe;
+        Fragment fragment ;
+        switch (v.getId()){
+            case vid:
+                fragment = new VideoPicFragment();
+                break;
+            case aboutsef:
+                fragment = new AboutMeFragment();
+                break;
+            case interest:
+                fragment = new InterestFragment();
+                break;
+            case friends:
+                fragment = new FriendsFragment();
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + v.getId());
+        }
+        if(fragment != null){
+            getChildFragmentManager().beginTransaction()
+                    .replace(profileFrame,fragment)
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                    .commit();
+        }
     }
 }
