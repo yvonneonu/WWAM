a25404aa58ea36830e9cc10f56bb0def91458a02
yvonneonu <yvonneonuorah@gmail.com>
2021-04-24 01:30:14 +0100

    Merge branch 'testpayment' into AccountProfile
    # Conflicts:
    #	app/src/main/java/com/example/waam/finalProfile.java

--- app/src/main/java/com/example/waam/picture.java
+++ app/src/main/java/com/example/waam/picture.java
@@ -1,11 +1,5 @@
 package com.example.waam;
 
-import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.FileProvider;
-
-import android.Manifest;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -19,6 +13,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import java.io.File;
 import java.io.IOException;
@@ -51,6 +50,7 @@ public class picture extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //bottom
+
             }
         });
 
