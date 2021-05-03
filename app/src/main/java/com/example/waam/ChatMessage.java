3b0852249b9122bb7a839f8fdb3ed71b04fe44ab
yvonneonu <yvonneonuorah@gmail.com>
2021-05-03 19:28:49 +0100

    Merge branch 'testpayment' into AccountProfile
    # Conflicts:
    #	app/src/main/java/com/example/waam/finalProfile.java

--- app/src/main/java/com/example/waam/ChatMessage.java
+++ app/src/main/java/com/example/waam/ChatMessage.java
@@ -2,6 +2,8 @@ package com.example.waam;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -22,17 +24,23 @@ public class ChatMessage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_message);
-        chats = GeneralFactory.getGeneralFactory().getChatList();
-        recyclerView = findViewById(R.id.recyclerView);
-        chatScreenAdapter = new ChatScreenAdapter(chats, ChatMessage.this);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(ChatMessage.this);
-        recyclerView.setAdapter(chatScreenAdapter);
-        recyclerView.setLayoutManager(linearLayoutManager);
+        String receiverId = getIntent().getStringExtra("receiversId");
+        ImageButton imageButtonSender = findViewById(R.id.imageButton);
+        generalFactoryInstance = GeneralFactory.getGeneralFactory(this);
+        generalFactoryInstance.loadMessages(chatCont -> {
+            chats = chatCont;
+            chatScreenAdapter = new ChatScreenAdapter(chats, ChatMessage.this);
+            recyclerView = findViewById(R.id.recyclerView);
+            LinearLayoutManager linearLayoutManager = new LinearLayoutManager(ChatMessage.this);
+            recyclerView.setAdapter(chatScreenAdapter);
+            recyclerView.setLayoutManager(linearLayoutManager);
+        },receiverId,ChatMessage.this);
 
-
-
-
-    }
+        imageButtonSender.setOnClickListener(v -> {
+            EditText editText = v.findViewById(R.id.edtMess);
+            String messages = editText.toString().trim();
+            generalFactoryInstance.sendMessage(messages,receiverId,ChatMessage.this);
+        });
 
     public void goback(View view) {
       finish();
