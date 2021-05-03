4b2d0ca1f85843ab83c7ecb7535c2b072acfcf73
yvonneonu <yvonneonuorah@gmail.com>
2021-05-03 19:37:39 +0100

    Merge branch 'testpayment' into AccountProfile
    # Conflicts:
    #	app/src/main/java/com/example/waam/finalProfile.java

--- app/src/main/java/com/example/waam/ChatMessage.java
+++ app/src/main/java/com/example/waam/ChatMessage.java
@@ -1,15 +1,12 @@
 package com.example.waam;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import com.connectycube.chat.model.ConnectycubeChatDialog;
 
 import java.util.List;
 
@@ -27,23 +24,23 @@ public class ChatMessage extends AppCompatActivity {
         setContentView(R.layout.activity_chat_message);
         String receiverId = getIntent().getStringExtra("receiversId");
         ImageButton imageButtonSender = findViewById(R.id.imageButton);
-        generalFactoryInstance = GeneralFactory.getGeneralFactory(this);
-        generalFactoryInstance.loadMessages(chatCont -> {
-            chats = chatCont;
+        //generalFactoryInstance = GeneralFactory.getGeneralFactory(this);
+      //  generalFactoryInstance.loadMessages(chatCont -> {
+         //   chats = chatCont;
             chatScreenAdapter = new ChatScreenAdapter(chats, ChatMessage.this);
             recyclerView = findViewById(R.id.recyclerView);
             LinearLayoutManager linearLayoutManager = new LinearLayoutManager(ChatMessage.this);
             recyclerView.setAdapter(chatScreenAdapter);
             recyclerView.setLayoutManager(linearLayoutManager);
-        },receiverId,ChatMessage.this);
+       // }://,receiverId,ChatMessage.this);
 
         imageButtonSender.setOnClickListener(v -> {
             EditText editText = v.findViewById(R.id.edtMess);
             String messages = editText.toString().trim();
-            generalFactoryInstance.sendMessage(messages,receiverId,ChatMessage.this);
+           // generalFactoryInstance.sendMessage(messages,receiverId,ChatMessage.this);
         });
 
-    public void goback(View view) {
-      finish();
+   // public void goback(View view) {
+    //  finish();
     }
 }
