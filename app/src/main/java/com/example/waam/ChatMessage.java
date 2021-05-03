ca10841a881e310117a9e0b347939521005c0859
yvonneonu <yvonneonuorah@gmail.com>
2021-05-03 19:59:29 +0100

    Merge branch 'testpayment' into AccountProfile
    # Conflicts:
    #	app/src/main/java/com/example/waam/finalProfile.java

--- app/src/main/java/com/example/waam/ChatMessage.java
+++ app/src/main/java/com/example/waam/ChatMessage.java
@@ -3,10 +3,17 @@ package com.example.waam;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.connectycube.chat.ConnectycubeChatService;
+import com.connectycube.chat.IncomingMessagesManager;
+import com.connectycube.chat.exception.ChatException;
+import com.connectycube.chat.listeners.ChatDialogMessageListener;
+import com.connectycube.chat.model.ConnectycubeChatMessage;
 
 import java.util.List;
 
@@ -21,6 +28,7 @@ public class ChatMessage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_message);
+        ImageButton imageButtonSender = findViewById(R.id.imageButton);
         chats = GeneralFactory.getGeneralFactory().getChatList();
         recyclerView = findViewById(R.id.recyclerView);
         chatScreenAdapter = new ChatScreenAdapter(chats, ChatMessage.this);
@@ -35,7 +43,24 @@ public class ChatMessage extends AppCompatActivity {
         imageButtonSender.setOnClickListener(v -> {
             EditText editText = v.findViewById(R.id.edtMess);
             String messages = editText.toString().trim();
-            generalFactoryInstance.sendMessage(messages,receiverId,ChatMessage.this);
+
+            ConnectycubeChatService chatService = ConnectycubeChatService.getInstance();
+            IncomingMessagesManager incomingMessagesManager = chatService.getIncomingMessagesManager();
+
+            messages = chatService.getIncomingMessagesManager("vguy");
+            incomingMessagesManager.addDialogMessageListener(new ChatDialogMessageListener() {
+                @Override
+                public void processMessage(String dialogId, ConnectycubeChatMessage message, Integer senderId) {
+
+                }
+
+                @Override
+                public void processError(String dialogId, ChatException exception, ConnectycubeChatMessage message, Integer senderId) {
+
+                }
+            });
+
+            //generalFactoryInstance.sendMessage(messages,receiverId,ChatMessage.this);
         });
 
     }
