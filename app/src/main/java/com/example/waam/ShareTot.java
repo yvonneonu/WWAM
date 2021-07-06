aea6b8e4d88b609999dc83ad3edbd296f64bf1b5
yvonneonu <yvonneonuorah@gmail.com>
2021-07-06 21:40:00 +0100

    Merge branch 'firebasewithagora' into videoPart
    # Conflicts:
    #	app/src/main/java/com/example/waam/CompleteProfile.java
    #	app/src/main/java/com/example/waam/Login.java
    #	app/src/main/java/com/example/waam/SignUpSecond.java

--- app/src/main/java/com/example/waam/ShareTot.java
+++ app/src/main/java/com/example/waam/ShareTot.java
@@ -4,25 +4,55 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class ShareTot extends AppCompatActivity implements View.OnClickListener {
-    private ImageView text2;
+    private ImageView text2, image;
     private ConstraintLayout coonc;
+    private TextView name;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share_tot);
 
+        String uid = FirebaseAuth.getInstance().getUid();
+        image = findViewById(R.id.imageView);
+
+
         coonc = findViewById(R.id.coonc);
         text2 = findViewById(R.id.text2);
+        name = findViewById(R.id.textView71);
+
+        GeneralFactory.getGeneralFactory(this).loadSpecUser(uid, new GeneralFactory.SpecificUser() {
+            @Override
+            public void loadSpecUse(WaamUser user) {
+                Glide.with(ShareTot.this)
+                        .asBitmap()
+                        .fitCenter()
+                        .circleCrop()
+                        .load(user.getImageUrl())
+                        .into(image);
+
+                name.setText(user.getFullname());
+            }
+        });
+
+
+
 
       text2.setOnClickListener(this);
+        text2.setColorFilter(Color.BLUE);
+
 
     }
 
@@ -30,7 +60,7 @@ public class ShareTot extends AppCompatActivity implements View.OnClickListener 
     @Override
     public void onClick(View v) {
         final int image = R.id.text2;
-        final int fram = R.id.fram1;
+        final int totFram = R.id.totFram;
 
         switch (v.getId()){
             case image:
@@ -38,7 +68,7 @@ public class ShareTot extends AppCompatActivity implements View.OnClickListener 
 
                 Fragment fragment = new TextdisplayFragment();
                 FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-                fragmentTransaction.replace(fram, fragment);
+                fragmentTransaction.replace(totFram, fragment);
                 fragmentTransaction.commit();
 
                 break;
