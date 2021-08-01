880ffa5187340c7aa08895aa1c9921c1339d1a2d
yvonneonu <yvonneonuorah@gmail.com>
2021-08-01 11:32:50 +0100

    Revert "Merge branch 'firebasewithagora' into videoPart"
    This reverts commit cfa445f5

--- app/src/main/java/com/example/waam/BookAgent.java
+++ app/src/main/java/com/example/waam/BookAgent.java
@@ -1,6 +1,8 @@
 package com.example.waam;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,5 +15,10 @@ public class BookAgent extends AppCompatActivity {
         setContentView(R.layout.activity_book_agent);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+    }
+
+    public void conti(View view) {
+        Intent intent = new Intent(BookAgent.this, AgentInterest.class);
+        startActivity(intent);
     }
 }
