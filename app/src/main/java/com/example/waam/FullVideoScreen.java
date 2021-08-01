7fe8bfce71b8720cac75396377e7c33c7b6fd01b
Bamidele Emmanuel <bamideleomonayin@gmail.com>
2021-08-01 16:09:57 +0100

    playing video or showing pics

--- app/src/main/java/com/example/waam/FullVideoScreen.java
+++ app/src/main/java/com/example/waam/FullVideoScreen.java
@@ -1,14 +1,42 @@
 package com.example.waam;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
 
 public class FullVideoScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_full_video_screen);
+        VideoPicModel videoPicModel = (VideoPicModel) getIntent().getSerializableExtra("picvic");
+        if(videoPicModel.isVideo()){
+            setContentView(R.layout.activity_full_video_screen);
+            VideoView videoView = findViewById(R.id.videoView2);
+            MediaController mediaController= new MediaController(this);
+            mediaController.setAnchorView(videoView);
+            //specify the location of media file
+            Uri uri=Uri.parse(videoPicModel.getVideoPicUrl());
+
+            //Setting MediaController and URI, then starting the videoView
+            videoView.setMediaController(mediaController);
+            videoView.setVideoURI(uri);
+            videoView.requestFocus();
+            videoView.start();
+        }else{
+            setContentView(R.layout.showfullpic);
+            ImageView imageView = findViewById(R.id.imageView75);
+            Glide.with(this)
+                    .asBitmap()
+                    .load(videoPicModel.getVideoPicUrl())
+                    .into(imageView);
+        }
+
     }
 }
